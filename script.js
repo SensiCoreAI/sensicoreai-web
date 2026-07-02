f148f70481f305f350e726c585c2ffa7022a7958
SensiCoreAI <mxapuestas48@gmail.com>
2026-07-01 22:31:12 -0500

    Add button click event for loading state

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("SensiCoreAI iniciado");
+const btn = document.getElementById("startBtn");
+
+btn.addEventListener("click", () => {
+
+    btn.innerText = "CARGANDO...";
+
+    btn.style.transform = "scale(0.95)";
+
+    setTimeout(() => {
+
+        btn.innerText = "COMENZAR";
+
+        btn.style.transform = "scale(1)";
+
+    },1000);
+
+});
